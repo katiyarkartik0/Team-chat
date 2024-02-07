c357d873c73dd63bfc652793def08c2879be4e7f
Kartik Katiyar <katiyarkartik0@gmail.com>
2024-02-08 01:25:58 +0530

    navbar made

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'cultured': '#F7F5F4',
+        'spanish-gray':"#A19791",
+        'neon-orange':"#FF731D"
+      },
+    }
+  },
+  plugins: [],
+}
+
